28a129d1dff1e68b7bd154dea759b4384d4461eb
Yury_Troyanov <Troyanov.yury@gmail.com>
2023-06-19 22:52:19 +0800

    ДЗ с первого урока на проверку

---
+++ Homework_1/Task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+string number_1 = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+string number_2 = Console.ReadLine();
+int num_1 = Convert.ToInt32(number_1);
+int num_2 = Convert.ToInt32(number_2);
+if (num_1 == num_2)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    if (num_1 > num_2)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+    else
+    {
+    Console.WriteLine("Второе число больше первого");
+    }
+}
