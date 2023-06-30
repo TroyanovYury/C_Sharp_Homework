8b135d4463ffcc2c99ba9e7ec81e12f99cef1256
Yury_Troyanov <Troyanov.yury@gmail.com>
2023-06-30 22:04:39 +0800

    Добавлены решения домашних заданий из 3 семинара

---
+++ Homework_3/Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.Write("Введите число для проверки: ");
+string number = Console.ReadLine();
+string reverse = string.Empty;
+
+for (int i = number.Length - 1; i >= 0; i--)
+{
+    reverse += number[i];
+}
+
+if (number == reverse)
+{
+    Console.WriteLine($"{number} является палиндромом.");
+}
+else
+{
+    Console.WriteLine($"{number} не палиндром.");
+}
+Console.ReadKey();
