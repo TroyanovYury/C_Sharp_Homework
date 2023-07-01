536f98bdf5560403230c41f7d8bcbadc33e819cf
Yury_Troyanov <Troyanov.yury@gmail.com>
2023-07-02 00:30:55 +0800

    ДЗ из 4 урока

---
+++ Homework_4/Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Напишите цикл, который принимает на 
+// вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int ReadInput(string msg)
+{
+    System.Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Clear();
+int a = ReadInput("Введите первое число: ");
+int b = ReadInput("Введите второе число: ");
+
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+    step = step * a;
+}
+Console.WriteLine("A в степени B равно: " + step);
---
+++ Homework_4/Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int ReadInput(string msg)
+{
+    System.Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Clear();
+
+int userNum = ReadInput("Введите число: ");
+
+int sum = 0;
+
+while (userNum > 0)
+{
+    sum += userNum % 10;
+    userNum = userNum / 10;
+}
+
+
+System.Console.WriteLine($"Сумма всех цифр равна {sum}");
